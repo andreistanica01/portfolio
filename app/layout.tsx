45bed5b0b406d216a453571fa3e52174967aa65b
Andrei Stanica <andrei.stanica03@gmail.com>
2026-02-18 22:53:50 +0200

    ss
    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,14 +31,17 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
+  
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
+    
     <html lang="en">
       <body className={`font-sans antialiased`}>
         {children}
+        <SpeedInsights/>
         <Analytics />
       </body>
     </html>
